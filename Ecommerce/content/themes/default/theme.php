48d6b9392a60b3edcc777198b5adc8bfa835309e
cakmoel <alanmoehammad@gmail.com>
2016-10-12 12:20:03 +0700

    commited to theme default

--- Ecommerce/content/themes/default/theme.php
+++ Ecommerce/content/themes/default/theme.php
@@ -16,7 +16,7 @@ if (!defined('PILUS_SHOP')) header("Location: 403.php");
 	
 $_SESSION['allow_access'] = true;
 
-statCollector();
+// statCollector();
 
 $metaOptions = "";
 
@@ -54,7 +54,7 @@ require_once ($getThemeActived->getTemplate_Folder() . '/widget.php');
 <meta name="webcrawlers" content="all" />
 <meta name="rating" content="general" />
 <meta name="spiders" content="all" />
-<meta name="generator" content="Bluefish 2.2.9" -" . PACK_VERSION; ?>" />
+<meta name="generator" content="Bluefish 2.2.9" -" . PACK_VERSION; ?>"
 
 <link rel="alternate" type="text/xml" title="<?php echo $siteName . " | " . $keywords; ?>"  href="content/feed" />
 <link href="<?php echo $getThemeActived -> getTemplate_Folder() . "/"; ?>css/templatemo_style.css" rel="stylesheet" type="text/css" />
